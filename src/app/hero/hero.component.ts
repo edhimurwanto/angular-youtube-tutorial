a1b390adf491cef8207a97e2c8f59be57834bcc8
Edi Murwanto <edhi.uchiha@gmail.com>
2021-07-11 08:24:00 +0700

    string interpolation

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -9,6 +9,20 @@ export class HeroComponent implements OnInit {
 
   title = 'Title hero component'
 
+  name = "Edi Murwanto"
+  hobbies = ["Sleeping", "Coding", "Swimming"]
+  number = 0
+
+  user = {
+    name: this.name,
+    age: 20,
+    address: "Jakarta"
+  }
+
+  getName(){
+    return this.name;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
