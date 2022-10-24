9a58b5a9ea4db4dab183c02568cfc4e9b6b5250c
Edi Murwanto <edhi.uchiha@gmail.com>
2022-10-24 08:25:13 +0700

    [CUSTOM DIRECTIVE]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
